5fb816b9ff28cf3537cfce13b8ae45f41f60dcf2
Mikko Matilainen <mikkom@iki.fi>
2019-03-23 09:45:27 +0200

    Try to prevent double-tap zoom on iOS

--- src/index.js
+++ src/index.js
@@ -4,5 +4,19 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Prevent double-tap zoom on iOS
+let lastTouchEnd = 0;
+document.addEventListener(
+  'touchend',
+  event => {
+    const now = new Date().getTime();
+    if (now - lastTouchEnd <= 500) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  },
+  false
+);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
